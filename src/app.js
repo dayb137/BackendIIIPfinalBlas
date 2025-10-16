9ee59e866bfa9a44adebfd6bc79569f218ebef7d
dayblas <dayblas007@gmail.com>
2025-10-16 15:39:05 -0300

    cree variables de entorono

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv"
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const MONGO_URI = process.env.MONGO_URI;
+
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,4 +24,12 @@ app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log("Conectado a MongoDB Atlas");
+        app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+    })
+    .catch(err => console.error("Error de conexión a Mongo:", err));
+
